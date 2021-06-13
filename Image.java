353181b05e38c45dc90e90d67acf2dec0987f889
henryd <henry.dossot@gmail.com>
2021-06-13 13:53:41 +0200

    Add files via upload

--- Image.java
+++ Image.java
@@ -2,6 +2,10 @@ package project;
 
 public class Image {
 	
+	// Declare Image class attributs 
+	int width, height, colorScale;
+	Segment [] segmentList; // Array list of segment which define the image 
+	
 	// Create method to display dimension image dimension
 	
 	// Create method to scrop image part between l1, l2, c1, c2
